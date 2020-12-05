c6355554d488d8cc21c3bcb9cc7c33c474a79a06
Mincong HUANG <mincong.h@gmail.com>
2020-12-05 10:04:17 +0100

    Test right

--- src/test/java/io/vavr/jackson/issues/Issue174Test.java
+++ src/test/java/io/vavr/jackson/issues/Issue174Test.java
@@ -1,10 +1,9 @@
 package io.vavr.jackson.issues;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vavr.control.Either;
-import io.vavr.jackson.datatype.BaseTest;
 import io.vavr.jackson.datatype.VavrModule;
 import org.junit.jupiter.api.Test;
 
@@ -26,12 +25,29 @@ class Issue174Test {
         assertEquals("Bianca", restored.getLeft().name);
     }
 
+    @Test
+    void eitherRight() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new VavrModule());
+
+        Dog dog = new Dog();
+        dog.name = "Labrador";
+        Either<Cat, Dog> either = Either.right(dog);
+
+        String json = objectMapper.writeValueAsString(either);
+        assertEquals("[\"right\",{\"name\":\"Labrador\"}]", json);
+
+        Either<Cat, Dog> restored = objectMapper.readValue(json, new TypeReference<Either<Cat, Dog>>() {});
+        assertEquals("Labrador", restored.get().name);
+    }
+
     static class Cat {
         @JsonProperty("name")
         String name;
     }
 
     static class Dog {
+        @JsonProperty("name")
         String name;
     }
 }
